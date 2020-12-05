13b2c82711ab079848038184d5a248164568ce0f
kiitoss <antoinechataignier1998@orange.fr>
2020-12-05 16:10:13 +0100

    first commit

---
+++ game/gameHeader.h
@@ -0,0 +1,151 @@
+#include "../mainHeader.h"
+
+#define NB_FRIENDS 3
+#define NB_ENEMIES 3
+#define NB_ROWS 5
+#define NB_COLUMNS 8
+/* MAX_ENEMIES est le nombre maximum d'enemies par ligne */
+#define MAX_ENEMIES 100
+/* MAX_SHOTS est le nombre maximum de flèches par ligne */
+#define MAX_SHOTS 100
+/* MAX_GOLDS est le nombre maximum de pièces d'or par ligne */
+#define MAX_GOLDS 100
+
+enum friend_abilities {ATTACk, DEFENSE, MONEY};
+typedef friend_abilities friend_abilities;
+
+typedef struct {
+  int posX;
+  int posY;
+  int width;
+  int height;
+} Container;
+
+typedef struct {
+  Container field;
+  Container friend_spawner;
+  Container enemy_spawner;
+  Container friend_home;
+  Container enemy_home;
+} Window;
+
+typedef struct {
+  int centerX;
+  int centerY;
+  int time_left;
+  int radius;
+  int max_radius;
+} Gold;
+
+typedef struct {
+  int posX;
+  int posY;
+  int width;
+  int attack;
+  int speed;
+} Shot;
+
+typedef struct {
+  char *name;
+  int chosen_friend;
+  int money;
+  char *money_str[10];
+  int last_free_gold;
+  int delay_free_gold;
+  int score;
+} Player_1;
+
+typedef struct {
+  char *name;
+  int chosen_enemy;
+  int chosen_row;
+  int money;
+  char *money_str[10];
+  int last_free_gold;
+  int delay_free_gold;
+  int score;
+} Player_2;
+
+typedef struct {
+  int id_friend;
+  MLV_Animation *animation_passive;
+  MLV_Animation *animation_ability;
+  friend_abilities ability;
+  int delay_frame_ability;
+  int life;
+  int attack;
+  int price;
+  char *price_str[5];
+  int delay_ability;
+  int range;
+} Friend_Spawner;
+
+typedef struct {
+  MLV_Animation *animation_attack;
+  MLV_Animation *animation_walking;
+  int life;
+  int attack;
+  int speed;
+  int price;
+  char *price_str[5];
+  int delay_frame_attack;
+  int delay_attack;
+} Enemy_Spawner;
+
+typedef struct {
+  int id_friend;
+  MLV_Animation *animation_passive;
+  MLV_Animation *animation_ability;
+  friend_abilities ability;
+  int delay_frame_ability;
+  int life;
+  int attack;
+  int delay_ability;
+  int range;
+  int posX;
+  bool is_passive;
+} Friend;
+
+typedef struct {
+  MLV_Animation *animation_attack;
+  MLV_Animation *animation_walking;
+  int life;
+  int attack;
+  int speed;
+  int delay_frame_attack;
+  int delay_attack;
+  int posX;
+  bool is_walking;
+} Enemy;
+
+typedef struct {
+  Friend friends[NB_COLUMNS];
+  Enemy enemies[MAX_ENEMIES];
+  int nb_enemies;
+  Shot shots[MAX_SHOTS];
+  int nb_shots;
+  Gold golds[MAX_GOLDS];
+  int nb_golds;
+  int rectsize;
+} Row;
+
+typedef struct {
+  Window window;
+  Friend_Spawner friend_spawners[NB_FRIENDS];
+  Enemy_Spawner enemy_spawners[NB_ENEMIES];
+  Row rows[NB_ROWS];
+  int rectsize;
+} Game_Manager;
+
+typedef struct {
+  MLV_Image *field_light_grass_img;
+  MLV_Image *field_dark_grass_img;
+  MLV_Image *enemy_spawner_background;
+  MLV_Image *friend_spawner_background;
+  MLV_Image *friend_home_background;
+  MLV_Image *enemy_home_background;
+  MLV_Image *friend_spawners_sprites[NB_FRIENDS];
+  MLV_Image *enemy_spawners_sprites[NB_ENEMIES];
+  MLV_Image *gold_img;
+  MLV_Image *shot_img;
+} Texture_Manager;
---
+++ mainHeader.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <MLV/MLV_all.h>
+
+
+enum bool {false, true};
+typedef bool bool;
+
+typedef struct {
+  MLV_Keyboard_button key_pressed;
+  int mouseX;
+  int mouseY;
+  MLV_Button_state button_state;
+} Event_Manager;
