4a720e366ece7b514cbad22c4b2de685f2f591e8
EINewstone <idrissbenguezzou@gmail.com>
2020-12-22 15:53:08 +0000

    feat(views): draw spaners

--- game/views/windowV.c
+++ game/views/windowV.c
@@ -2,14 +2,29 @@
 #include "../makhead.h"
 
 void draw_home(Game_Manager *GM, Texture_Manager *TM) {
-  
+  MLV_draw_image(TM->friend_home_background,GM->window.friend_home.posX, GM->window.friend_home.posY);
+  MLV_draw_image(TM->enemy_home_background,GM->window.enemy_home.posX, GM->window.enemy_home.posY); 
 }
 
 void draw_friend_spawner(Game_Manager *GM, Texture_Manager *TM) {
+  int i;
+
+  MLV_draw_image(TM->friend_spawner_background,GM->window.friend_spawner.posX, GM->window.friend_spawner.posY);
+  for (i=0;i<NB_FRIENDS;i++){
+    MLV_draw_image(TM->friend_spawners_imgs[i],GM->window.friend_spawner.posX + i*GM->window.rectsize,GM->window.friend_spawner.posY);
+  }
+  
   
 }
 
 void draw_enemy_spawner(Game_Manager *GM, Texture_Manager *TM) {
+  int i;
+
+  MLV_draw_image(TM->enemy_spawner_background,GM->window.enemy_spawner.posX, GM->window.enemy_spawner.posY);
+
+  for (i=0;i<NB_ENEMIES;i++){
+    MLV_draw_image(TM->enemy_spawners_imgs[i],GM->window.enemy_spawner.posX + i*GM->window.rectsize,GM->window.enemy_spawner.posY);
+  }
   
 }
 
@@ -27,7 +42,8 @@ void draw_field(Game_Manager *GM, Texture_Manager *TM) {
 /* GLOBAL */
 void draw_window(Game_Manager *GM, Texture_Manager *TM) {
   draw_home(GM, TM);
-  draw_friend_spawner(GM, TM);
+  draw_friend_spawner(GM,TM);
+  GM->gamemode = MULTI; 
   if (GM->gamemode == MULTI) {
     draw_enemy_spawner(GM, TM);
   }
